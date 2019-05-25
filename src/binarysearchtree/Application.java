b480ec543f51839eeed3ee9861bbec27f8b109e9
sayaka-tamura <arccycle@gmail.com>
2019-05-26 01:28:39 +0900

    Add Remove method on Binary Search Tree

--- src/binarysearchtree/Application.java
+++ src/binarysearchtree/Application.java
@@ -15,5 +15,7 @@ public class Application {
 		System.out.println(tree.findMin().key);
 		System.out.println(tree.findMax().key);
 
+		System.out.println(tree.remove(10)); // print true
+		System.out.println(tree.findMin().key);
 	}
 }
--- src/binarysearchtree/BST.java
+++ src/binarysearchtree/BST.java
@@ -58,4 +58,92 @@ public class BST {
 		}
 		return last;
 	}
+
+	public boolean remove(int key) {
+
+		Node currentNode = root;
+		Node parentNode = root;
+
+		boolean isLeftChild = false;
+
+		// Searching to find the node with the key to delete
+		while (currentNode.key != key) {
+			parentNode = currentNode;
+			if (key < currentNode.key) {
+				isLeftChild = true;
+				currentNode = currentNode.leftchild;
+			} else {
+				currentNode = currentNode.rightchild;
+				isLeftChild = false;
+			}
+			if (currentNode == null)
+				return false;
+		}
+
+		// found the node
+		Node nodeToDelete = currentNode;
+
+		// Case 1: if node is a leaf
+		if (nodeToDelete.leftchild == null && nodeToDelete.rightchild == null) {
+			if (nodeToDelete == root)
+				root = null;
+			else if (isLeftChild)
+				parentNode.leftchild = null;
+			else
+				parentNode.rightchild = null;
+		}
+
+		// Case 2-1 : if node has one child that is on the left
+		else if (nodeToDelete.rightchild == null) {
+			if (nodeToDelete == root)
+				root = nodeToDelete.leftchild;
+			else if (isLeftChild)
+				parentNode.leftchild = nodeToDelete.leftchild;
+			else
+				parentNode.rightchild = nodeToDelete.leftchild;
+		}
+		// Case 2-2 :if node has one child that is on the right
+		else if (nodeToDelete.leftchild == null) {
+			if (nodeToDelete == root)
+				root = nodeToDelete.rightchild;
+			else if (isLeftChild)
+				parentNode.leftchild = nodeToDelete.rightchild;
+			else
+				parentNode.rightchild = nodeToDelete.rightchild;
+		}
+
+		// Case 3 :if node has 2 children (tricky)
+		else {
+			Node successor = getSuccessor(nodeToDelete);
+			// connect parent of nodeToDelete to successor instead
+			if (nodeToDelete == root)
+				root = successor;
+			else if (isLeftChild)
+				parentNode.leftchild = successor;
+			else
+				parentNode.rightchild = successor;
+
+			successor.leftchild = nodeToDelete.leftchild;
+		}
+		return true;
+	}
+
+	private Node getSuccessor(Node nodeToDelete) {
+		Node successorParent = nodeToDelete;
+		Node successor = nodeToDelete;
+
+		Node current = nodeToDelete.rightchild; // Go to the right child
+
+		while (current != null) { // start going left down the tree until node has no left child
+			successorParent = successor;
+			successor = current;
+			current = current.leftchild;
+		}
+		// successor not a right child
+		if (successor != nodeToDelete.rightchild) {
+			successorParent.leftchild = successor.rightchild;
+			successor.rightchild = nodeToDelete.rightchild;
+		}
+		return successor;
+	}
 }
